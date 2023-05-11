430197f2e0ecd712ff7afcfe24e084681ef3f8fa
Luca Molteni <volothamp@gmail.com>
2023-05-11 14:47:32 +0200

    Subcommands

--- ruleops-extension/deployment/src/main/java/org/drools/ruleops/extension/deployment/RuleopsExtensionProcessor.java
+++ ruleops-extension/deployment/src/main/java/org/drools/ruleops/extension/deployment/RuleopsExtensionProcessor.java
@@ -1,5 +1,7 @@
 package org.drools.ruleops.extension.deployment;
 
+import java.util.HashMap;
+
 import javax.inject.Inject;
 
 import io.quarkus.arc.deployment.GeneratedBeanBuildItem;
@@ -10,12 +12,14 @@ import io.quarkus.deployment.pkg.builditem.OutputTargetBuildItem;
 import io.quarkus.gizmo.ClassCreator;
 import io.quarkus.gizmo.ClassOutput;
 import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 
 class RuleopsExtensionProcessor {
 
     private static final String FEATURE = "ruleops-extension";
-    public final String TOP_COMMAND_NAME = "org.drools.cliexample.ExampleTopCommand";
+    public static final String TOP_COMMAND_NAME = "org.drools.cliexample.ExampleTopCommand";
+    public static final String COMMAND_1_NAME = "org.drools.cliexample.Command1";
 
     @Inject
     OutputTargetBuildItem outputTargetBuildItem;
@@ -36,19 +40,39 @@ class RuleopsExtensionProcessor {
             generatedBean.produce(new GeneratedBeanBuildItem(name, data));
         };
 
+
         try (ClassCreator classCreator = ClassCreator.builder()
                 .classOutput(classOutput)
-                .className(TOP_COMMAND_NAME)
+                .className(COMMAND_1_NAME)
                 .interfaces(Runnable.class)
                 .build()) {
 
             classCreator.getMethodCreator("run", "void").returnVoid();
 
+            AnnotationInstance annotation =
+                    AnnotationInstance.builder(DotName.createSimple(picocli.CommandLine.Command.class))
+                            .add("name", "commandname")
+                            .build();
+            classCreator.addAnnotation(annotation);
+        }
+
+        try (ClassCreator classCreator = ClassCreator.builder()
+                .classOutput(classOutput)
+                .className(TOP_COMMAND_NAME)
+                .build()) {
+
             classCreator.addAnnotation(io.quarkus.picocli.runtime.annotations.TopCommand.class);
+
+            DotName[] empty = new DotName[]{};
+            ClassInfo subCommandName = ClassInfo.create(DotName.createSimple(COMMAND_1_NAME),
+                                                        DotName.createSimple(""),
+                                                        (short) 0, empty, new HashMap<>(), true);
+
 
             AnnotationInstance annotation =
                     AnnotationInstance.builder(DotName.createSimple(picocli.CommandLine.Command.class))
                             .add("mixinStandardHelpOptions", true)
+                            .add("subcommands", new ClassInfo[]{ subCommandName })
                             .build();
             classCreator.addAnnotation(annotation);
         }
